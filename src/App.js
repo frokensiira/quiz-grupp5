12b08cb4a42c6dbbed5fd6c54f25c55e58b9f220
My Siira <my_siira@hotmail.com>
2020-03-03 19:00:25 +0100

    first draft boilerplate for quiz and homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
 import React from 'react';
-import { db } from './modules/firebase'
+import football from './assets/football.jpg'
+import map from './assets/map.png'
+import music from './assets/music.jpg'
+import dog from './assets/dog.jpg'
+//import { db } from './modules/firebase'
 
 class App extends React.Component {
   render() {
 
     return (
       <div className="App">
-  
+
+        <div class="py-3 bg-white text-center">
+          <div class="container">
+              <h2 class="text-primary display-3 my-4">Quiz-titel</h2>
+          </div>
+        </div>
+
+        <div class="result py-4 d-none bg-light text-center">
+          <div class="container lead">
+              <p>Du fick<span class="text-primary display-4 p-3">0</span> rätt</p>
+          </div>
+        </div>
+
+        <div class="quiz py-4 bg-primary">
+          <div class="container">
+              <h2 class="my-5 text-white">On with the questions...</h2>
+
+              <form class="quiz-form text-light">
+
+                  <div class="my-5">
+                    <p class="lead font-weight-normal">1. How do you give a ninja directions?</p>
+                    <div class="form-check my-2 text-white">
+                        <input type="radio" name="q1" value="A" checked className="mr-2"/>
+                        <label class="form check-label">Show them the map</label>
+                    </div>
+                    <div class="form-check my-2 text-white">
+                        <input type="radio" name="q1" value="B" className="mr-2"/>
+                        <label class="form check-label">Dont' worry, the ninja will find you</label>
+                    </div>
+                  </div>
+
+                  <div class="my-5">
+                    <p class="lead font-weight-normal">2. How do you give a ninja directions?</p>
+                    <div class="form-check my-2 text-white">
+                        <input type="radio" name="q2" value="A" checked className="mr-2"/>
+                        <label class="form check-label">Show them the map</label>
+                    </div>
+                    <div class="form-check my-2 text-white">
+                        <input type="radio" name="q2" value="B" className="mr-2"/>
+                        <label class="form check-label">Dont' worry, the ninja will find you</label>
+                    </div>
+                  </div>
+
+                  <div class="my-5">
+                    <p class="lead font-weight-normal">3. How do you give a ninja directions?</p>
+                    <div class="form-check my-2 text-white">
+                        <input type="radio" name="q3" value="A" checked className="mr-2"/>
+                        <label class="form check-label">Show them the map</label>
+                    </div>
+                    <div class="form-check my-2 text-white">
+                        <input type="radio" name="q3" value="B" className="mr-2"/>
+                        <label class="form check-label">Dont' worry, the ninja will find you</label>
+                    </div>
+                  </div>
+
+                  <div class="text-center">
+                      <input type="submit" class="btn btn-light" value="Skicka svar"/>
+                  </div>
+
+              </form>
+          </div>
+        </div>
       </div>
+
     );
 
   }
