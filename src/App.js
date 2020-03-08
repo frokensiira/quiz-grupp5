46892b6cd3aa97ecc3e2d7e53fb7c5f2db4b4098
cod01 <nasir.haidari@hotmail.com>
2020-03-08 12:38:44 +0100

    commit before pull from gtihub02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-import AddQuiz from './modules/AddQuiz';
-import QuizItem from './components/QuizItem'
-import { db } from './modules/firebase';
-// import { Authentication } from './modules/firebase';
-import LogIn from './components/LogIn'
-
-=======
 import AddQuiz from './components/AddQuiz';
 import { Route, BrowserRouter, Link, Switch } from 'react-router-dom'
 import ShowQuiz from './components/ShowQuiz'
 import QuizContainer from './components/QuizContainer'
->>>>>>> master
 
 
 class App extends React.Component {
@@ -38,24 +29,6 @@ class App extends React.Component {
               </div>
             </div>
           </div>
-<<<<<<< HEAD
-        </div>
-
-        <div className="quiz py-4 bg-primary">
-          <div className="container">
-            <div className="row row-cols-1 row-cols-md-2 mt-5">
-            <LogIn />
-            {quizItem}
-              <AddQuiz /> 
-            </div>
-          </div>
-        </div>
-        
-      </div> 
-=======
-
-          
->>>>>>> master
 
           <Switch>
             <Route exact path='/' component={QuizContainer}/>
